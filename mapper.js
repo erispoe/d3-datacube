1790ec1f662ae9e4e0f7e79052ede1f0d65609b8
Erispoe <favrebulle@gmail.com>
2014-11-15 15:21:45 -0800

    mapper.get()
    get a specific d3.map

--- mapper.js
+++ mapper.js
@@ -19,6 +19,7 @@ d3.mapper = function module() {
     exports.array = function(_a, _id) {
       _id = typeof _id !== 'undefined' ? _id : 'id';
       //TODO: check if all arrays have the same keys
+      //TODO: returns an array representation of the mapper when _a is not specified
       var keys = Object.keys(_a[0]);
       keys.splice(keys.indexOf(_id), 1);
 
@@ -31,6 +32,13 @@ d3.mapper = function module() {
         _m = d3.map(_o);
         this.add(_m, _v);
       };
+      return this;
+    }
+
+    exports.get = function(_v, _y){
+      // return a specific d3.map
+      _y = typeof _y !== 'undefined' ? _y : 'null';
+      if(_maps[_v][_y]){return _maps[_v][_y]};
       return this;
     }
 
