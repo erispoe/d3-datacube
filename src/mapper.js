c43e5e073fd6dadfaec78b4188233319c4202d60
Erispoe <favrebulle@gmail.com>
2014-12-05 10:39:56 -0800

    Module organization

--- src/mapper.js
+++ src/mapper.js
@@ -1,9 +1,21 @@
-d3.mapper = function module() {
+d3.mapper = function() {
+  var m = new d3_mapper;
+  return m;
+}
+
+function d3_mapper() {
 
 	var _maps = {};
 
 	function exports() {}
     
+    /**
+    * Adds a d3.map to the mapper
+    * @param {d3.map} _m - The d3.map to add to the mapper
+    * @param {string} _v - The name of the variable mapped in the d3.map
+    * @param {d3.map} _y - The year or numerical series oredring of the variable mapped in the d3.map
+    * @return {d3.mapper} - The d3.mapper with the d3.map added to it
+    **/
     exports.add = function(_m, _v, _y) {
     	_y = typeof _y !== 'undefined' ? _y : 'null';
       if(!_maps[_v]){_maps[_v] = {}}
@@ -11,7 +23,12 @@ d3.mapper = function module() {
     	return this;
     }
 
+    exports.mergeWith = function(_m) {
+
+    }
+
     exports.array = function(_a, _id) {
+      // Create the mapper object from an array representation
       if (!arguments.length) return this._getArray;
       _id = typeof _id !== 'undefined' ? _id : 'id';
       //TODO: check if all arrays have the same keys
